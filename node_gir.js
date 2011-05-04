d6404af0d4cf3db24132ed92b35708ed0399ec1a
Tim Caswell <tim@creationix.com>
2011-05-03 17:39:41 -0700

    Partial progress, don't use as is

--- node_gir.js
+++ node_gir.js
@@ -1,2 +1,5 @@
 var NodeGirNative = require('./build/default/node_gir_native.node');
-console.dir(NodeGirNative);
+NodeGirNative.gTypeInit();
+//console.dir(NodeGirNative);
+//console.dir(NodeGirNative.prependSearchPath("Foobar"));
+//console.dir(NodeGirNative.getSearchPath());
