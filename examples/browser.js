d70009ee9ef40aa728df425658accf2ab97dfcda
Sebastian Wick <wick.sebastian@gmail.com>
2011-09-17 18:59:40 +0200

    use a already loaded namespace when no specific version is given; give more information to the JS land; api improvements

--- examples/browser.js
+++ examples/browser.js
@@ -1,22 +1,19 @@
-var gir = require("../gir");
-gir.init();
-var gtk = gir.load("Gtk", "3.0");
-
-var WebKit = require("./webkit");
+var gtk = require("./gtk"),
+    WebKit = require("./webkit");
 
 gtk.init(0);
 
 var win = new gtk.Window();
 
 var sw = new gtk.ScrolledWindow();
-win.__call__("add", sw);
+win.add(sw);
 
 var view = new WebKit.WebView();
-view.__call__("load_uri", "http://www.google.com/");
-sw.__call__("add", view);
+view.loadUri("http://www.google.com/");
+sw.add(view);
 
-win.__call__("set_size_request", 640, 480);
-win.__call__("show_all");
+win.setSizeRequest(640, 480);
+win.showAll();
 
 gtk.main();
 
