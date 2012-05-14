87e854322335a3ba40586182fd5b7e671f9c1816
piotras <piotrek.pokora@gmail.com>
2012-05-14 09:05:58 +0200

    Simplify function invokation. Get rid of CamelCase conversion

--- src/types/function.cc
+++ src/types/function.cc
@@ -12,94 +12,32 @@ using namespace v8;
 
 namespace gir {
 
-/*
-FIXME: this is reeeealy ugly. we should find onother way to find the namespace and function name.
-       and/or even consider if we just create a __call__ function like we do in object.cc
-*/
-
 void GIRFunction::Initialize(Handle<Object> target, GIObjectInfo *info, const char *namespace_) 
 {
-    HandleScope scope;
-   
-    const char *fname = g_base_info_get_name(info);
-    const gchar *ns = g_base_info_get_namespace(info);
-    char *name = new char[strlen(namespace_) + strlen(fname) + 2];
-    strcpy(name, namespace_);
-    name[strlen(namespace_)] = ':';
-    strcpy(name+strlen(namespace_)+1, fname);
-    
+    const char *func_name = g_base_info_get_name(info);
+    // Create new function
     Local<FunctionTemplate> temp = FunctionTemplate::New(Execute);
-    temp->GetFunction()->Set(String::New("__fname__"), String::New(name));
-    temp->GetFunction()->SetName(String::New(name));
-
-    char *jsname = ToCamelCase(g_base_info_get_name(info));
-    target->Set(String::NewSymbol(fname), temp->GetFunction());
-
-    delete[] jsname;
-    delete[] name;
-}
-
-char *GIRFunction::ToCamelCase(const char *str) 
-{
-    int l = strlen(str), it = 0;
-    char *res = new char[l];
-    
-    bool up = false;
-    for(int i=0; i<l; i++) {
-        if(str[i] == '_') {
-            up = true;
-        }
-        else if(!up) {
-            res[it++] = str[i];
-        }
-        else {
-            res[it++] = str[i] - 'a' + 'A';
-            up = false;
-        }
-    }
-    res[it] = '\0';
-    return res;
+    // Set name
+    temp->GetFunction()->SetName(String::New(func_name));
+    // Create external to hold GIBaseInfo and set it
+    v8::Handle<v8::External> info_ptr = v8::External::New((void*)g_base_info_ref(info));
+    temp->GetFunction()->SetHiddenValue(String::New("GIInfo"), info_ptr);
+    // Set symbol
+    target->Set(String::NewSymbol(func_name), temp->GetFunction());
 }
 
 Handle<Value> GIRFunction::Execute(const Arguments &args) 
 {
     HandleScope scope;
-    
-    //String::Utf8Value fname(args.Callee()->Get(String::New("__fname__")));
-    String::Utf8Value fname(args.Callee()->GetName());
-    String::AsciiValue func_name(args.Callee()->GetName());
+    // Get GIFunctionInfo pointer    
+    v8::Handle<v8::External> info_ptr =
+        v8::Handle<v8::External>::Cast(args.Callee()->GetHiddenValue(String::New("GIInfo")));
+    GIBaseInfo *func  = (GIBaseInfo*) info_ptr->Value();
 
-    int l=strlen(*fname);
-    char *ns = NULL;
-    char *fn = NULL;
-    for(int i=0; i<l; i++) {
-        if((*fname)[i] == ':') {
-            
-            ns = new char[i];
-            for(int j=0; j<i; j++) {
-                ns[j] = (*fname)[j];
-            }
-            ns[i] = '\0';
-            
-            fn = new char[l-i];
-            for(int j=0; j<(l-i); j++) {
-                fn[j] = (*fname)[i+j+1];
-            }
-            fn[l-i] = '\0';
-            
-            break;
-        }
-    }
-   
-    printf("EXECUTE '%s' '%s' '%s' '%s' \n", *fname, ns, fn, *func_name);
-
-    if (ns == NULL || fn == NULL) {
-        return EXCEPTION("Unknown namespace or function");
-    }
-
-    GIFunctionInfo *func = g_irepository_find_by_name(NamespaceLoader::repo, ns, fn);
-    delete[] ns;
-    delete[] fn;
+    printf("EXECUTE namespace: '%s',  name: '%s', symbol: '%s' \n", 
+            g_base_info_get_namespace(func),
+            g_base_info_get_name(func),
+            g_function_info_get_symbol(func));
 
     if(func) {
         return scope.Close(Func::Call(NULL, func, args, TRUE));
