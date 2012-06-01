4a7aed150a4b0fcbb0c245ee2c515a9362078f97
piotras <piotrek.pokora@gmail.com>
2012-06-01 17:06:54 +0200

    More simple crud tests

--- midgard/test_object_crud.js
+++ midgard/test_object_crud.js
@@ -20,8 +20,40 @@ suite.addBatch({
             topic : function () {
                 return MidgardTest.TestBook.create()
             },
-            'should be true' : function (topic) {
-                assert.isTrue (topic)
+            'is created' : function (topic) {
+                assert.isTrue(topic);
+            },	
+            'is valid guid' : function (topic) {
+                assert.isTrue(Midgard.is_guid(MidgardTest.TestBook.guid));
+            }	
+        },
+        'Update' : {
+            topic : function () {
+                MidgardTest.TestBook.name = "The Holy Grail 2";
+                return MidgardTest.TestBook.update()
+            },
+            'is updated' : function (topic) {
+                assert.isTrue(topic);
+            },	
+            'is valid guid' : function (topic) {
+                assert.isTrue(Midgard.is_guid(MidgardTest.TestBook.guid));
+            }	
+        },
+        'Delete' : {
+            topic : function () { 
+                return MidgardTest.TestBook.delete(false)
+            },
+            'is deleted' : function (topic) {
+                assert.isTrue(topic);
+            }
+        },
+        'Purge' : {
+            topic : function () {
+                MidgardTest.TestBook.create();
+                return MidgardTest.TestBook.purge(false)
+            },
+            'is purged' : function (topic) {
+                assert.isTrue(topic);
             }	
         }
     }
