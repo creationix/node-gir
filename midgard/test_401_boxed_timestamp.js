22a98dbd8ebca2350d836cc298236af1c5c1ae89
piotras <piotrek.pokora@gmail.com>
2012-06-13 17:26:42 +0200

    Initial to test boxed type

---
+++ midgard/test_401_boxed_timestamp.js
@@ -0,0 +1,135 @@
+
+var vows   = require('vows'),
+    assert = require('assert');
+
+var gir = require('../gir');
+gir.init();
+
+var GObject = require('./gobject');
+
+var Midgard, gir, config, mgd;
+Midgard = gir.load('Midgard');
+Midgard.init();
+
+var MidgardTest = require('./midgard_connection');
+
+// Test G_TYPE_BOXED
+// metadata.created is C structure hold in GValue of boxed type
+
+var suite = vows.describe('Midgard.Timestamp (Boxed)');
+suite.addBatch({
+    'Get Default' : {
+        'Year' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isNumber(topic.year);
+                assert.equal(topic.year, 0);
+            }	
+        },
+        'Month' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isNumber(topic.month);
+                assert.equal(topic.month, 0);
+            }	
+        },
+        'Day' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) { 
+                assert.isNumber(topic.day);
+                assert.equal(topic.day, 0);
+            }	
+        },
+        'Hour' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isNumber(topic.hour);
+                assert.equal(topic.hour, 0);
+            }	
+        },
+        'Minute' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isNumber(topic.minute);
+                assert.equal(topic.minute, 0);
+            }	
+        },
+        'Second' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isNumber(topic.second);
+                assert.equal(topic.second, 0);
+            }	
+        },
+        'get_string' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                assert.isString(topic.get_string());
+                assert.equal(topic.get_string(), "0000-00-00 00:00:00+0000");
+            }	
+        }
+    },
+    'Set & Get' : {
+        'Year' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 2012' : function (topic) {
+                topic.year = 2012;
+                assert.isNumber(topic.year);
+                assert.equal(topic.year, 2012);
+            }	
+        },
+        'Month' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 6' : function (topic) {
+                topic.month = 6;
+                assert.isNumber(topic.month);
+                assert.equal(topic.month, 6);
+            }	
+        },
+        'Day' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 7' : function (topic) {
+                topic.day = 7;
+                assert.isNumber(topic.day);
+                assert.equal(topic.day, 7);
+            }	
+        },
+        'Hour' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 14' : function (topic) {
+                topic.hour = 14;
+                assert.isNumber(topic.hour);
+                assert.equal(topic.hour, 14);
+            }	
+        },
+        'Minute' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 35' : function (topic) {
+                topic.minute = 35;
+                assert.isNumber(topic.minute);
+                assert.equal(topic.minute, 35);
+            }	
+        },
+        'Second' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                topic.second = 0;
+                assert.isNumber(topic.second);
+                assert.equal(topic.second, 0);
+            }	
+        },
+        'get_string' : {
+            topic : MidgardTest.TestBook.metadata.created,
+            'is 0' : function (topic) {
+                topic.year = 2012;
+                topic.month = 6;
+                topic.day = 7;
+                topic.hour = 14;
+                topic.minute = 35;
+                topic.second = 0;
+                assert.isString(topic.get_string());
+                assert.equal(topic.get_string(), "2012-06-07 14:35:00+0000");
+            }	
+        }
+    }
+}).run();
