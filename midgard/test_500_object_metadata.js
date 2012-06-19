d7b5fb0d6b1998a00281e928fbee2e34334652ef
piotras <piotrek.pokora@gmail.com>
2012-06-19 17:24:13 +0200

    Updated for mocha

--- midgard/test_500_object_metadata.js
+++ midgard/test_500_object_metadata.js
@@ -1,90 +1,147 @@
 
-var vows   = require('vows'),
+var mocha = require('mocha'),
     assert = require('assert');
 
 var gir = require('../gir');
 gir.init();
 
-var GObject = require('./gobject');
-
-var Midgard, gir, config, mgd;
-Midgard = gir.load('Midgard');
+var GObject = gir.load('GObject');
+var Midgard = gir.load('Midgard');
 Midgard.init();
 
 var MidgardTest = require('./midgard_connection');
 
-var suite = vows.describe('Midgard.Metadata');
-suite.addBatch({
-    'CRUD' : {
-        'Create' : {
-            topic : function () {
-                var mdata = MidgardTest.TestBook.metadata;
-                mdata.score = -1;
-                mdata.authors = "Sir Lancelot and Knights";
-                mdata.hidden = false;
-                MidgardTest.TestBook.create();
-                return MidgardTest.TestBook.metadata;
-            },
-            'is metadata' : function (topic) {
-                assert.isObject(topic);
-            },	
-            'test score' : function (topic) {
-                assert.equal(topic.score, -1);
-            },	
-            'test authors' : function (topic) {
-                assert.equal(topic.authors, "Sir Lancelot and Knights");
-            },
-            'test hidden' : function (topic) {
-                assert.isFalse(topic.hidden);
-            },
-            'test created' : function (topic) {
-                assert.isNotNull(topic.created.year);
-            }
-        },
-        'Update' : {
-            topic : function () {
-                var mdata = MidgardTest.TestBook.metadata;
-                mdata.score = 1;
-                mdata.authors = "Sir Lancelot and Knights and King Arthur";
-                mdata.hidden = true;              
-                MidgardTest.TestBook.update();
-                return MidgardTest.TestBook.metadata;
-            },
-            'is metadata' : function (topic) {
-                assert.isObject(topic);
-            },	
-            'test score' : function (topic) {
-                assert.equal(topic.score, 1);
-            },	
-            'test authors' : function (topic) {
-                assert.equal(topic.authors, "Sir Lancelot and Knights and King Arthur");
-            },
-            'test hidden' : function (topic) {
-                assert.isTrue(topic.hidden);
-            },
-            'test revised' : function (topic) {
-                assert.equal("TODO", "DONE");
-            }
-        },
-        'Delete' : {
-            topic : function () { 
-                MidgardTest.TestBook.delete(false);
-                return MidgardTest.TestBook.metadata;
-            },
-            'is deleted' : function (topic) {
-                assert.isTrue(topic.deleted);
-            }
-        },
-        'Purge BookStore' : {
-            topic : function () {
-                return MidgardTest.TestBookStore.purge(false)
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is purged' : function (topic) {
-                assert.isTrue(topic);
-            }	
-        }
-    }
-}).run();
+var mdata = MidgardTest.TestBook.metadata;
+mdata.score = -1;
+mdata.authors = "Sir Lancelot and Knights";
+mdata.hidden = false;
+var published = mdata.published;
+published.year = 2012;
+published.month = 7;
+published.day = 1; 
+var datestring = "2012-07-01 00:00:00+0000";
+MidgardTest.TestBook.metadata.published = published;
+MidgardTest.TestBook.create();
+
+
+describe('Midgard.Metadata', function() {
+    
+    describe('Create', function() {
+    
+        it('is metadata', function () {
+            assert.isObject(MidgardTest.TestBook.metadata);
+        });
+        
+        it('test score', function () {
+            assert.equal(MidgardTest.TestBook.metadata.score, -1);
+        });
+        
+        it('test authors', function () {
+            assert.equal(MidgardTest.TestBook.metadata.authors, "Sir Lancelot and Knights");
+        });
+        
+        it('test hidden', function () {
+            assert.isFalse(MidgardTest.TestBook.metadata.hidden);
+        });
+        
+        it('test created', function () {
+            var year = MidgardTest.TestBook.metadata.created.year;
+            assert.isNotNull(year);
+            var d = new Date();
+            assert.equal(year, d.getFullYear());
+        });
+        
+        it('test creator', function () {
+            var creator = MidgardTest.TestBook.metadata.creator;
+            assert.isNotNull(creator);
+            assert.equal(creator, "");
+        });
+        
+        it('test revised', function () {
+            var year = MidgardTest.TestBook.metadata.revised.year;
+            assert.isNotNull(year);
+            var d = new Date();
+            assert.equal(year, d.getFullYear());
+        });
+        
+        it('test revisor', function () {
+            var revisor = MidgardTest.TestBook.metadata.revisor;
+            assert.isNotNull(revisor);
+            assert.equal(revisor, "");
+        });
+        
+        it('test published', function () {
+            var year = MidgardTest.TestBook.metadata.published.year;
+            var month =  MidgardTest.TestBook.metadata.published.month;
+            var day =  MidgardTest.TestBook.metadata.published.day;
+            assert.isNotNull(year);
+            assert.isNotNull(month);
+            assert.isNotNull(day);
+            assert.equal(year, 2012);
+            assert.equal(month, 7);
+            assert.equal(day, 1);
+            assert.equal(MidgardTest.TestBook.metadata.published.get_string(), datestring);
+        });
+    });
+
+    describe('Update', function() {
+    
+        it('is updated', function () {
+            var mdata = MidgardTest.TestBook.metadata;
+            mdata.score = 1;
+            mdata.authors = "Sir Lancelot and Knights and King Arthur";
+            mdata.hidden = true;              
+            MidgardTest.TestBook.update();
+            assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
+            assert.isObject(MidgardTest.TestBook.metadata);
+            assert.isObject(mdata);
+        });
+
+        it('test score', function () {
+            assert.equal(MidgardTest.TestBook.metadata.score, 1);
+        });
+        
+        it('test authors', function () {
+            assert.equal(MidgardTest.TestBook.metadata.authors, "Sir Lancelot and Knights and King Arthur");
+        });
+        
+        it('test hidden', function () {
+            assert.isTrue(MidgardTest.TestBook.metadata.hidden);
+        });
+        
+        it('test revised', function () {
+            var revised = MidgardTest.TestBook.metadata.revised;
+            assert.isNotNull(revised);
+            var d = new Date();
+            assert.equal(revised.year, d.getFullYear());
+            assert.equal(revised.month-1, d.getMonth()); // Data,getMonth() range is 0-11
+            assert.equal(revised.day, d.getDate());
+        });
+        
+        it('test creator', function () {
+            var creator = MidgardTest.TestBook.metadata.creator;
+            assert.isNotNull(creator);
+            assert.equal(creator, "");
+        });
+
+    });
+
+    describe('Delete', function() {
+    
+        it('is deleted', function () {
+            var deleted = MidgardTest.TestBook.delete(false);
+            assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
+            assert.isTrue(deleted);
+            assert.isTrue(MidgardTest.TestBook.metadata.deleted);
+        });
+    });
+    describe('Purge', function() {
+    
+        it('is purged', function () {
+            var purged = MidgardTest.TestBook.purge(false);
+            assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
+            assert.isTrue(purged);
+            assert.isTrue(MidgardTest.TestBook.metadata.deleted);
+        });
+    });
+});
