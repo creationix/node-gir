7d294014f615c403fb3b236c205e54397f96271d
piotras <piotrek.pokora@gmail.com>
2012-06-21 09:59:09 +0200

    Use mocha and should

--- midgard/test_storage.js
+++ midgard/test_storage.js
@@ -1,102 +1,73 @@
 
-var vows   = require('vows'),
-    assert = require('assert');
+var mocha = require('mocha'),
+    should = require('should');
 
 var gir = require('../gir');
 gir.init();
 
 var GObject = gir.load('GObject');
+var Midgard = gir.load('Midgard');
 
-var Midgard, gir, config, mgd;
-Midgard = gir.load('Midgard');
-Midgard.init();
+var MidgardTest = require('./midgard_connection');
 
-var mgd = require('./midgard_connection');
+describe('Midgard.Storage', function() {
 
-var suite = vows.describe('Midgard.Storage');
-suite.addBatch({
-    'Storage' : {
-        'Create base' : {
-            topic : function () {
-                return Midgard.Storage.create_base_storage(mgd.cnc);
-            },
-            'created' : function (topic) {
-                assert.isTrue (topic)
-            }	
-        },
-        'Create MgdSchema' : {
-            topic : function () {
-                var type = GObject.type_from_name('MidgardObject');
-                children = GObject.type_children(type);
-                for (i in children) {
-                    //console.log("Create " + GObject.type_name(children[i]) + " storage");
-                    if (!Midgard.Storage.create(mgd.cnc, GObject.type_name(children[i]))) {
-                        return false;
-                    }
-                }
-                return true;
-            },
-            'created' : function (topic) {
-                assert.isTrue (topic)
-            }	
-        },
-        'Create DBObject' : {
-            topic : function () {
-                var type = GObject.type_from_name('MidgardDBObject');
-                children = GObject.type_children(type);
-                var ignored = {'MidgardMetadata':1, 'MidgardObject':1, 'MidgardView':1};
-                for (i in children) {
-                    var typename = GObject.type_name(children[i]);
-                    if (ignored[typename]) {
-                        continue;
-                    }
-                    //console.log("Create " + GObject.type_name(children[i]) + " storage");
-                    if (!Midgard.Storage.create(mgd.cnc, GObject.type_name(children[i]))) {
-                        return false;
-                    }
-                }
-                return true;
-            },
-            'created' : function (topic) {
-                assert.isTrue (topic)
-            }	
-        },
-        'Update MgdSchema' : {
-            topic : function () {
-                var type = GObject.type_from_name('MidgardObject');
-                children = GObject.type_children(type);
-                for (i in children) {
-                    //console.log("Create " + GObject.type_name(children[i]) + " storage");
-                    if (!Midgard.Storage.update(mgd.cnc, GObject.type_name(children[i]))) {
-                        return false;
-                    }
-                }
-                return true;
-            },
-            'created' : function (topic) {
-                assert.isTrue (topic)
-            }	
-        },
-        'Update DBObject' : {
-            topic : function () {
-                var type = GObject.type_from_name('MidgardDBObject');
-                children = GObject.type_children(type);
-                var ignored = {'MidgardMetadata':1, 'MidgardObject':1, 'MidgardView':1};
-                for (i in children) {
-                    var typename = GObject.type_name(children[i]);
-                    if (ignored[typename]) {
-                        continue;
-                    }
-                    //console.log("Create " + GObject.type_name(children[i]) + " storage");
-                    if (!Midgard.Storage.update(mgd.cnc, GObject.type_name(children[i]))) {
-                        return false;
-                    }
-                }
-                return true;
-            },
-            'created' : function (topic) {
-                assert.isTrue (topic)
-            }	
+    it('create base storage', function() {
+        var created = Midgard.Storage.create_base_storage(MidgardTest.cnc);
+        created.should.equal(true);
+    });
+
+    it('create MgdSchema storage', function() {
+        var type = GObject.type_from_name('MidgardObject');
+        var children = GObject.type_children(type);
+        var i = 0;
+        for (i in children) {
+            //console.log("Create " + GObject.type_name(children[i]) + " storage");
+             var created = Midgard.Storage.create(MidgardTest.cnc, GObject.type_name(children[i]));
+             created.should.equal(true);
         }
-    }
-}).run();
+    });
+   
+    it('create DBObject storage', function() {
+        var type = GObject.type_from_name('MidgardDBObject');
+        var children = GObject.type_children(type);
+        var i = 0;
+        var ignored = {'MidgardMetadata':1, 'MidgardObject':1, 'MidgardView':1};
+        for (i in children) {
+            var typename = GObject.type_name(children[i]);
+            if (ignored[typename]) {
+                continue;
+            }
+            //console.log("Create " + GObject.type_name(children[i]) + " storage");
+            var created = Midgard.Storage.create(MidgardTest.cnc, GObject.type_name(children[i]));
+            created.should.equal(true);
+        }
+    });
+         
+    it('update MgdSchema storage', function() {
+        var type = GObject.type_from_name('MidgardObject');
+        var children = GObject.type_children(type);
+        var i = 0;
+        for (i in children) {
+            //console.log("Create " + GObject.type_name(children[i]) + " storage");
+            var updated = Midgard.Storage.update(MidgardTest.cnc, GObject.type_name(children[i]));
+            updated.should.equal(true);
+        }
+    });
+    
+    it('update DBObject storage', function() {
+        var type = GObject.type_from_name('MidgardDBObject');
+        var children = GObject.type_children(type);
+        var i = 0;
+        var ignored = {'MidgardMetadata':1, 'MidgardObject':1, 'MidgardView':1};
+        for (i in children) {
+            var typename = GObject.type_name(children[i]);
+            if (ignored[typename]) {
+                continue;
+            }
+            //console.log("Create " + GObject.type_name(children[i]) + " storage");
+            var updated = Midgard.Storage.create(MidgardTest.cnc, GObject.type_name(children[i]));
+            updated.should.equal(true);
+        }
+    }); 
+}); 
