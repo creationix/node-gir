f43ce3111c57aab33f4a66766ea1da57901e2375
piotras <piotrek.pokora@gmail.com>
2012-06-21 14:11:40 +0200

    Initial

---
+++ midgard/test_query_100_column.js
@@ -0,0 +1,41 @@
+
+var mocha = require('mocha'),
+    should = require('should');
+
+var gir = require('../gir');
+gir.init();
+
+var GObject = gir.load('GObject');
+var Midgard = gir.load('Midgard');
+Midgard.init();
+
+var MidgardTest = require('./midgard_connection');
+var column;
+var property;
+
+describe('Midgard.SqlQueryColumn', function() {
+
+    it('is instance', function() {
+        property = new Midgard.QueryProperty({'property':'title'})
+        column = new Midgard.SqlQueryColumn({'queryproperty':property, 'name':'The title', 'qualifier':'t1'});
+        column.should.be.a('object');
+        //column.should.be.an.instanceof(Midgard.SqlQueryColumn);
+    });
+
+    it('has queryproperty', function() {
+        var prop = column.get_query_property();
+        prop.should.equal(property);
+        prop.property.should.equal(property.property);
+    });
+
+    it('has name', function() {
+        var name = column.get_name();
+        name.should.equal('The title');
+    });
+
+    it('has qualifier', function() {
+        var q = column.get_qualifier();
+        q.should.equal('t1');
+    });
+
+});
