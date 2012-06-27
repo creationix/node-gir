f7255c6bc4a457fb91d9efcd750c21c18bffd165
piotras <piotrek.pokora@gmail.com>
2012-06-27 15:30:32 +0200

    Use mocha

--- midgard/test_401_boxed_timestamp.js
+++ midgard/test_401_boxed_timestamp.js
@@ -1,14 +1,12 @@
 
-var vows   = require('vows'),
-    assert = require('assert');
+var mocha = require('mocha'),
+            should = require('should');
 
 var gir = require('../gir');
 gir.init();
 
 var GObject = gir.load('GObject');
-
-var Midgard, gir, config, mgd;
-Midgard = gir.load('Midgard');
+var Midgard = gir.load('Midgard');
 Midgard.init();
 
 var MidgardTest = require('./midgard_connection');
@@ -16,155 +14,211 @@ var MidgardTest = require('./midgard_connection');
 // Test G_TYPE_BOXED
 // metadata.created is C structure hold in GValue of boxed type
 
-var suite = vows.describe('Midgard.Timestamp (Boxed)');
-suite.addBatch({
-    'Get Default' : {
-        'Year' : {
-            'Created' : {
-                topic : MidgardTest.TestBook.metadata.created,
-                'is 1' : function (topic) {
-                    assert.isNumber(topic.year);
-                    assert.equal(topic.year, 1);
-                }
-            },
-             'Revised' : {
-                topic : MidgardTest.TestBook.metadata.revised,
-                'is 1' : function (topic) {
-                    assert.isNumber(topic.year);
-                    assert.equal(topic.year, 1);
-                }
-            }
-        },
-        'Month' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 1' : function (topic) {
-                assert.isNumber(topic.month);
-                assert.equal(topic.month, 1);
-            }	
-        },
-        'Day' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 1' : function (topic) { 
-                assert.isNumber(topic.day);
-                assert.equal(topic.day, 1);
-            }	
-        },
-        'Hour' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                assert.isNumber(topic.hour);
-                assert.equal(topic.hour, 0);
-            }	
-        },
-        'Minute' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                assert.isNumber(topic.minute);
-                assert.equal(topic.minute, 0);
-            }	
-        },
-        'Second' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                assert.isNumber(topic.second);
-                assert.equal(topic.second, 0);
-            }	
-        },
-        'get_string' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                assert.isString(topic.get_string());
-                assert.equal(topic.get_string(), "0001-01-01 00:00:00+0000");
-            }	
-        }
-    },
-    'Set Year' : {
-        'Created' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 2012' : function (topic) {
-                topic.year = 2012;
-                assert.isNumber(topic.year);
-                assert.equal(topic.year, 2012);
-            }	
-        },
-        'Revised' : {
-            topic : MidgardTest.TestBook.metadata.revised,
-            'is 2013' : function (topic) {
-                topic.year = 2013;
-                assert.isNumber(topic.year);
-                assert.equal(topic.year, 2013);
-            }	
-        },
-        'Published' : {
-            topic : MidgardTest.TestBook.metadata.published,
-            'is 2014' : function (topic) {
-                topic.year = 2014;
-                assert.isNumber(topic.year);
-                assert.equal(topic.year, 2014);
-            }	
-        }
-    },
-    'Set & Get' : {
-        'Year' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 2012' : function (topic) {
-                topic.year = 2012;
-                assert.isNumber(topic.year);
-                assert.equal(topic.year, 2012);
-            }	
-        },
-        'Month' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 6' : function (topic) {
-                topic.month = 6;
-                assert.isNumber(topic.month);
-                assert.equal(topic.month, 6);
-            }	
-        },
-        'Day' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 7' : function (topic) {
-                topic.day = 7;
-                assert.isNumber(topic.day);
-                assert.equal(topic.day, 7);
-            }	
-        },
-        'Hour' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 14' : function (topic) {
-                topic.hour = 14;
-                assert.isNumber(topic.hour);
-                assert.equal(topic.hour, 14);
-            }	
-        },
-        'Minute' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 35' : function (topic) {
-                topic.minute = 35;
-                assert.isNumber(topic.minute);
-                assert.equal(topic.minute, 35);
-            }	
-        },
-        'Second' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                topic.second = 0;
-                assert.isNumber(topic.second);
-                assert.equal(topic.second, 0);
-            }	
-        },
-        'get_string' : {
-            topic : MidgardTest.TestBook.metadata.created,
-            'is 0' : function (topic) {
-                topic.year = 2012;
-                topic.month = 6;
-                topic.day = 7;
-                topic.hour = 14;
-                topic.minute = 35;
-                topic.second = 0;
-                assert.isString(topic.get_string());
-                assert.equal(topic.get_string(), "2012-06-07 14:35:00+0000");
-            }	
-        }
-    }
-}).run();
+describe('Midgard.TimeStamp (Boxed)', function() {
+
+    describe('Get default values', function() {
+
+        it ('year', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.year.should.be.a('number');
+            v.year.should.equal(1);
+        });
+
+        it ('month', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.month.should.be.a('number');
+            v.month.should.equal(1);
+        });
+
+        it ('day', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.day.should.be.a('number');
+            v.day.should.equal(1);
+        });
+
+        it ('hour', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.hour.should.be.a('number');
+            v.hour.should.equal(0);
+        });
+
+        it ('minute', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.minute.should.be.a('number');
+            v.minute.should.equal(0);
+        });
+
+        it ('second', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.second.should.be.a('number');
+            v.second.should.equal(0);
+        });
+
+        it ('get string', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.get_string().should.be.a('string');
+            v.get_string().should.equal('0001-01-01 00:00:00+0000');
+        });
+    });
+
+    describe('Set year', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.year = 2012;
+            v.year.should.be.a('number');
+            v.year.should.equal(2012);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.year = 2013;
+            v.year.should.be.a('number');
+            v.year.should.equal(2013);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.year = 2014;
+            v.year.should.be.a('number');
+            v.year.should.equal(2014);
+        });
+
+    });
+
+    describe('Set month', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.month = 1;
+            v.month.should.be.a('number');
+            v.month.should.equal(1);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.month = 2;
+            v.month.should.be.a('number');
+            v.month.should.equal(2);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.month = 3;
+            v.month.should.be.a('number');
+            v.month.should.equal(3);
+        });
+
+    });
+
+    describe('Set day', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.day = 1;
+            v.day.should.be.a('number');
+            v.day.should.equal(1);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.day = 2;
+            v.day.should.be.a('number');
+            v.day.should.equal(2);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.published;
+            v.day = 3;
+            v.day.should.be.a('number');
+            v.day.should.equal(3);
+        });
+    });
+
+    describe('Set hour', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.hour = 1;
+            v.hour.should.be.a('number');
+            v.hour.should.equal(1);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.hour = 2;
+            v.hour.should.be.a('number');
+            v.hour.should.equal(2);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.published;
+            v.hour = 3;
+            v.hour.should.be.a('number');
+            v.hour.should.equal(3);
+        });
+    });
+
+    describe('Set minute', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.minute = 1;
+            v.minute.should.be.a('number');
+            v.minute.should.equal(1);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.minute = 2;
+            v.minute.should.be.a('number');
+            v.minute.should.equal(2);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.published;
+            v.minute = 3;
+            v.minute.should.be.a('number');
+            v.minute.should.equal(3);
+        });
+    });
+
+    describe('Set second', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.second = 1;
+            v.second.should.be.a('number');
+            v.second.should.equal(1);
+        });
+
+        it ('revised', function() {
+            var v = MidgardTest.TestBook.metadata.revised;
+            v.second = 2;
+            v.second.should.be.a('number');
+            v.second.should.equal(2);
+        });
+
+        it ('published', function() {
+            var v = MidgardTest.TestBook.metadata.published;
+            v.second = 3;
+            v.second.should.be.a('number');
+            v.second.should.equal(3);
+        });
+    });
+
+    describe('Get string', function() {
+
+        it ('created', function() {
+            var v = MidgardTest.TestBook.metadata.created;
+            v.year = 2012;
+            v.month = 6;
+            v.day = 7;
+            v.hour = 14;
+            v.minute = 35;
+            v.second = 0;
+            v.get_string().should.be.a('string');
+            v.get_string().should.equal('2012-06-07 14:35:00+0000');
+        });
+    });
+});
