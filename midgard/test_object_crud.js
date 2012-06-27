40617769476c063cd09bdcab8f4bcc3ee1d1c39b
piotras <piotrek.pokora@gmail.com>
2012-06-27 16:21:52 +0200

    use mocha

--- midgard/test_object_crud.js
+++ midgard/test_object_crud.js
@@ -1,83 +1,133 @@
 
-var vows   = require('vows'),
-    assert = require('assert');
+var mocha = require('mocha'),
+    should = require('should');
 
 var gir = require('../gir');
 gir.init();
 
 var GObject = gir.load('GObject');
-
-var Midgard, gir, config, mgd;
-Midgard = gir.load('Midgard');
+var Midgard = gir.load('Midgard');
 Midgard.init();
 
 var MidgardTest = require('./midgard_connection');
 
-var suite = vows.describe('Midgard.Object');
-suite.addBatch({
-    'CRUD' : {
-        'Create' : {
-            topic : function () {
-                return MidgardTest.TestBook.create()
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is created' : function (topic) {
-                assert.isTrue(topic);
-            },	
-            'is valid guid' : function (topic) {
-                assert.isTrue(Midgard.is_guid(MidgardTest.TestBook.guid));
-            }	
-        },
-        'Update' : {
-            topic : function () {
-                MidgardTest.TestBook.name = "The Holy Grail 2";
-                return MidgardTest.TestBook.update()
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is updated' : function (topic) {
-                assert.isTrue(topic);
-            },	
-            'is valid guid' : function (topic) {
-                assert.isTrue(Midgard.is_guid(MidgardTest.TestBook.guid));
-            }	
-        },
-        'Delete' : {
-            topic : function () { 
-                return MidgardTest.TestBook.delete(false)
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is deleted' : function (topic) {
-                assert.isTrue(topic);
-            }
-        },
-        'Purge Book' : {
-            topic : function () {
-                MidgardTest.TestBook.create();
-                return MidgardTest.TestBook.purge(false)
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is purged' : function (topic) {
-                assert.isTrue(topic);
-            }	
-        },
-        'Purge BookStore' : {
-            topic : function () {
-                return MidgardTest.TestBookStore.purge(false)
-            },
-            'check error' : function (topic) {
-                assert.equal(MidgardTest.cnc.get_error_string(), "MGD_ERR_OK");
-            },	
-            'is purged' : function (topic) {
-                assert.isTrue(topic);
-            }	
-        }
-    }
-}).run();
+describe('Midgard.Object CRUD', function() {
+
+    describe('Create', function() {
+       
+        var book = MidgardTest.TestBook;
+
+        it('is created', function() {
+            var created = book.create();
+            created.should.be.a('boolean');
+            created.should.equal(true);
+        });
+
+        it('check error code', function() {
+            var error_code = MidgardTest.cnc.get_error();
+            error_code.should.equal(Midgard.GenericError.ok);
+        });
+
+        it('check error string', function() {
+            var error_string = MidgardTest.cnc.get_error_string();
+            error_string.should.equal("MGD_ERR_OK");
+        });
+
+        it('is valid guid', function() {
+            var valid_guid = true;
+            valid_guid.should.equal(Midgard.is_guid(book.guid));
+        });
+
+    });
+
+    describe('Update', function() {
+       
+        var book = MidgardTest.TestBook;
+        book.title = 'The Holy Grail 2';
+
+        it('is updated', function() {
+            var updated = book.update();
+            updated.should.be.a('boolean');
+            updated.should.equal(true);
+        });
+
+        it('check error code', function() {
+            var error_code = MidgardTest.cnc.get_error();
+            error_code.should.equal(Midgard.GenericError.ok);
+        });
+
+        it('check error string', function() {
+            var error_string = MidgardTest.cnc.get_error_string();
+            error_string.should.equal("MGD_ERR_OK");
+        });
+
+        it('is valid guid', function() {
+            var valid_guid = true;
+            valid_guid.should.equal(Midgard.is_guid(book.guid));
+        });
+    });
+
+    describe('Delete', function() {
+       
+        var book = MidgardTest.TestBook;
+
+        it('is deleted', function() {
+            var deleted = book.delete(false);
+            deleted.should.be.a('boolean');
+            deleted.should.equal(true);
+        });
+
+        it('check error code', function() {
+            var error_code = MidgardTest.cnc.get_error();
+            error_code.should.equal(Midgard.GenericError.ok);
+        });
+
+        it('check error string', function() {
+            var error_string = MidgardTest.cnc.get_error_string();
+            error_string.should.equal("MGD_ERR_OK");
+        });
+    });
+
+    describe('Purge book', function() {
+       
+        var book = MidgardTest.TestBook;
+
+        it('is purged', function() {
+            var purged = book.purge(false);
+            purged.should.be.a('boolean');
+            purged.should.equal(true);
+        });
+
+        it('check error code', function() {
+            var error_code = MidgardTest.cnc.get_error();
+            error_code.should.equal(Midgard.GenericError.ok);
+        });
+
+        it('check error string', function() {
+            var error_string = MidgardTest.cnc.get_error_string();
+            error_string.should.equal("MGD_ERR_OK");
+        });
+    });
+
+    describe('Purge bookstore', function() {
+       
+        var book = MidgardTest.TestBookStore;
+
+        it('is purged', function() {
+            var purged = book.purge(false);
+            purged.should.be.a('boolean');
+            purged.should.equal(true);
+        });
+
+        it('check error code', function() {
+            var error_code = MidgardTest.cnc.get_error();
+            error_code.should.equal(Midgard.GenericError.ok);
+        });
+
+        it('check error string', function() {
+            var error_string = MidgardTest.cnc.get_error_string();
+            error_string.should.equal("MGD_ERR_OK");
+        });
+    });
+
+});
