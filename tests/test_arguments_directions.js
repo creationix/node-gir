a23ddf1b048c7994ca85b59a0c67543f34ab0283
piotras <piotrek.pokora@gmail.com>
2012-06-29 09:44:04 +0200

    Use mocha

--- tests/test_arguments_directions.js
+++ tests/test_arguments_directions.js
@@ -1,74 +1,81 @@
 
-var vows   = require('vows'),
-    assert = require('assert');
+var mocha = require('mocha'),
+    should = require('should');
 
 var gir = require('../gir');
 gir.init();
+
+var GLib = gir.load('GLib');
+var GObject = gir.load('GObject');
 
 // Let's use gtk window for testing purpose
 var gtk = require("./gtk");
 gtk.init(0);
 
 var gdkPixbuf = require("./gdkPixbuf");
-var glib = require("./glib");
-var gobject = require("./gobject");
 
 var objects = require('./objects');
 var win = objects.win;
 
-var suite = vows.describe('Arguments direction');
-suite.addBatch({
-	'Argument' : {
-		'in' : {
-			topic: win, 
-			'integer': function (topic) {
-				topic.set_property("default_height", 1);
-			},
-			'string': function (topic) {
-				topic.set_property("title", "Lancelot");
-			},
-			'boolean': function (topic) {
-				topic.set_property("modal", true);
-			},
-			'double': function (topic) {
-                assert.equal("TODO", "DONE");
-			},
-			'null': function (topic) {
-                topic.set_icon(null);
-			},
-			'object': function (topic) {
-                pixbuf = new gdkPixbuf.Pixbuf(0, false, 1, 1, 1);
-                topic.set_icon(pixbuf);
-			}
-		},
-		'out' : {
-			topic: win, 
-			'get_property': function (topic) {
-				topic.set_title("Lancelot");
-				var title = null;
-				topic.get_property("title", title);
-				assert.equal(title, "Lancelot");
-				assert.notEqual(title, "");
-				assert.notEqual(title, null);
+describe('Arguments direction', function() {
 
-			},
-			'integer': function (topic) {
-                var type = gobject.type_from_name("GtkWindow");
-                var children = gobject.type_children(type); // hidden, implicit array length
-                assert.isNotNull(children);
-				assert.notEqual(children.length, 0);
-                assert.include(children, gobject.type_from_name("GtkDialog"));
-                assert.include(children, gobject.type_from_name("GtkApplicationWindow"));
-                assert.include(children, gobject.type_from_name("GtkAssistant"));
-                assert.include(children, gobject.type_from_name("GtkPlug"));
-                assert.include(children, gobject.type_from_name("GtkOffscreenWindow"));
-			}
-		},
-		'in out' : {
-			topic: win, 
-			'integer': function (topic) {
-                assert.equal("TODO", "DONE");
-			}
-		}
-	}
-}).export(module);
+    describe('in', function() {
+
+        it('integer', function() {
+            win.set_property("default_height", 1);
+        });
+
+        it('string', function() {
+            win.set_property("title", "Lancelot");
+        });
+
+        it('boolean', function() {
+            win.set_property("modal", true);
+        });
+
+        it('double', function() {
+            var done = "TODO";
+            done.should.equal("DONE");
+        });
+
+        it('null', function() {
+            win.set_icon(null);
+        });
+
+        it('object', function() {
+            var pixbuf = new gdkPixbuf.Pixbuf(0, false, 1, 1, 1);
+            win.set_icon(pixbuf);
+        });
+    });
+
+    describe('out', function() {
+
+        it('get_property', function() {
+            win.set_title("Lancelot");
+            var title = null;
+            win.get_property("title", title);
+            title.should.equal("Lancelot");
+        });
+          
+	    it('integer', function() {
+            var type = GObject.type_from_name("GtkWindow");
+            var children = GObject.type_children(type); // hidden, implicit array length
+            //children.should.not.equal(null);
+            children.length.should.not.be.below(1);
+            children.should.include(GObject.type_from_name('GtkDialog'));
+            children.should.include(GObject.type_from_name('GtkApplicationWindow'));
+            children.should.include(GObject.type_from_name('GtkAssistant'));
+            children.should.include(GObject.type_from_name('GtkPlug'));
+            children.should.include(GObject.type_from_name('GtkOffscreenWindow'));
+		});
+    });
+
+    describe('in out', function() {
+        
+        it('integer', function() {
+            var done = "TODO";
+            done.should.equal("DONE");
+        });
+    });
+});
+
