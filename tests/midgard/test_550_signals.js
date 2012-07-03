cb2580483eecbfe1d7baa854ef16e7c6ed5dcddc
piotras <piotrek.pokora@gmail.com>
2012-07-03 11:32:42 +0200

    Renamed so execution os done when database is created

--- tests/midgard/test_550_signals.js
+++ tests/midgard/test_550_signals.js
@@ -12,7 +12,9 @@ Midgard.init();
 var config = new Midgard.Config();
 config.read_file_at_path('test_SQLITE.conf');
 
-var mgd = new Midgard.Connection();
+var MidgardTest = require('./midgard_connection');
+
+var mgd = MidgardTest.cnc
 var from_connection_callback = '';
 var from_object_callback = '';
 var obj = null;
@@ -28,19 +30,17 @@ describe('Signals', function() {
     
     describe('Midgard.Connection', function() {
   
-        it('connected', function() {
-            mgd.open_config(config);
-            from_connection_callback.should.equal('MGD_ERR_OK');
-            from_connection_callback = '';
-        });
-
-        it('error', function() {
+        it('create object error', function() {
             obj = Midgard.Object.factory(mgd, "gir_test_book_store");
             obj.create();
             from_connection_callback.should.equal('MGD_ERR_OK');
+        });
+
+        it('purge object error', function() {
             obj.purge(false);
             from_connection_callback.should.equal('MGD_ERR_OK');
         });
+
     });
 
     describe('Midgard.Object', function() {
@@ -52,6 +52,16 @@ describe('Signals', function() {
             });
             obj.create();
             from_object_callback.should.equal('Object create');
+            from_connection_callback.should.equal('MGD_ERR_OK');
+        });
+
+        it('action-purge', function() {
+            obj = Midgard.Object.factory(mgd, "gir_test_book_store");
+            obj.on('action-purge', function() {
+                from_object_callback = 'Object purge';
+            });
+            obj.purge('false');
+            from_object_callback.should.equal('Object purge');
             from_connection_callback.should.equal('MGD_ERR_OK');
         });
     });
