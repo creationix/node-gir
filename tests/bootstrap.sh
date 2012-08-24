ff25350ea719a6309c745fb7fc7c3aca77d1a45e
piotras <piotrek.pokora@gmail.com>
2012-08-24 13:48:58 +0200

    Involve mocha instead of vows

--- tests/bootstrap.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-npm install vows
-npm install gir
-# Create symlink in case npm is too old
-ln -s default node_modules/gir/build/Release
--- tests/test_object_property.js
+++ tests/test_object_property.js
@@ -1,75 +1,83 @@
 
-var vows   = require('vows'),
-    assert = require('assert');
+var mocha = require('mocha'),
+    should = require('should');
 
 var TestGIR = require('./TestGIR');
 var win = TestGIR.win;
 var GdkPixbuf = TestGIR.GdkPixbuf;
 
-var suite = vows.describe('Gtk.Object');
-suite.addBatch({
-	'Property' : {
-		'boolean' : {
-			topic: win, 
-			'should be true': function (topic) {
-				topic.modal = true;
-				assert.isTrue(topic.modal);
-			},
-			'should be false': function (topic) {
-				topic.modal = false;
-				assert.isFalse(topic.modal);
-			}
-		},
-		'string' : {
-			topic: win, 
-			'set Lancelot': function (topic) {
-				topic.title = "Lancelot";
-				assert.equal(topic.title, "Lancelot");
-			},
-			'get Lancelot': function (topic) {
-				assert.equal(topic.title, "Lancelot");
-				assert.notEqual(topic.title, "");
-				assert.notEqual(topic.title, " ");
-				assert.notEqual(topic.title, null);
-			}
-		},
-		'integer' : {
-			topic: win, 
-			'set 1': function (topic) {
-				topic['default-height'] = 1;
-				assert.equal(topic['default-height'], 1);
-			},
-			'get 1': function (topic) {
-				assert.equal(topic['default-height'], 1);
-				assert.notEqual(topic['default-height'], 0);
-				assert.notEqual(topic['default-height'], 2);
-				assert.notEqual(topic['default-height'], -1);
-			}
-		},
-		'double' : {
-			topic: win, 
-			'set 0.33': function (topic) {
-				topic.opacity = 0.33;
-				assert.equal(topic.opacity, 0.33);
-			},
-			'get 0.33': function (topic) {
-				assert.equal(topic.opacity, 0.33);
-				assert.notEqual(topic.opacity, 0.001);
-				assert.notEqual(topic.opacity, 2);
-				assert.notEqual(topic.opacity, 1.23);
-			}
-		},
-		'object' : {
-			topic: win, 
-			'set object': function (topic) {
-				var pixbuf = new GdkPixbuf.Pixbuf(0, false, 1, 1, 1);
-				topic.icon = pixbuf;
-				assert.equal(topic.icon, pixbuf);
-			},
-			'get object': function (topic) {
-				assert.notEqual(topic.icon, null);
-				assert.notEqual(topic.icon, topic);
-			}
-		}
-	}
-}).run();
+var pixbuf = new GdkPixbuf.Pixbuf(0, false, 1, 1, 1);
+
+describe('Gtk.Object', function() {
+
+    describe('property', function() {
+        
+        describe('boolean', function() {
+
+            it('should be true', function() {
+                win.modal = true;
+                win.modal.should.equal(true);
+            });
+            
+            it('should be false', function() {
+                win.modal = false;
+                win.modal.should.equal(false);
+            });
+        });
+
+        describe('string', function() {
+
+            it('set Lancelot', function() {
+                win.title = 'Lancelot';
+                win.title.should.equal('Lancelot');
+            });
+            
+            it('get Lancelot', function() {
+                win.title.should.equal('Lancelot');
+                win.title.should.not.equal('');
+                win.title.should.not.equal(' ');
+            });
+        });
+
+        describe('integer', function() {
+
+            it('set 1', function() {
+                win['default-height'] = 1;
+                win['default-height'].should.equal(1);
+            });
+            
+            it('get 1', function() {
+                win['default-height'].should.equal(1);
+                win['default-height'].should.not.equal(0);
+                win['default-height'].should.not.equal(-1);
+            });
+        });
+
+        describe('double', function() {
+
+            it('set 0.33', function() {
+                win.opacity = 0.33;
+                win.opacity.should.equal(0.33);
+            });
+            
+            it('get 0.33', function() {
+                win.opacity.should.equal(0.33);
+                win.opacity.should.not.equal(0.001);
+                win.opacity.should.not.equal(2);
+                win.opacity.should.not.equal(1.23);
+            });
+        });
+
+        describe('object', function() {
+
+            it('set icon', function() { 
+                win.icon = pixbuf;
+            });
+            
+            it('get icon', function() {
+                win.icon.should.be.a('object');
+                win.icon.should.not.equal(win);
+            });
+        });
+    });
+});
