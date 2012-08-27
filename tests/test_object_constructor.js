50ba320c3a2746f98f151d7e3cfe536555c4a3c3
Jon Nordby <jononor@gmail.com>
2012-08-28 00:11:43 +0200

    tests: Add tests for Object.new_for() style construction functions

--- tests/test_object_constructor.js
+++ tests/test_object_constructor.js
@@ -8,17 +8,47 @@ gir.init();
 var GLib = gir.load('GLib');
 var GObject = gir.load('GObject');
 
-var gtk = gir.load('Gtk', '3.0');
-gtk.init(0);
+var Gtk = gir.load('Gtk', '3.0');
+Gtk.init(0);
 
 describe('Object constructor', function() {
+
+    describe('with "new" operator', function() {
+
+        it('without arguments', function() {
+            var label = new Gtk.Label();
+            label.label.should.equal("");
+        });
+
+        it('with properties', function() {
+            var label = new Gtk.Label( { label: 'aText99'} );
+            label.label.should.equal('aText99');
+        });
+    });
+
+    describe('using static functions', function() {
+
+        /* FIXME: pending. Calling .new() ends up calling GObject.Object.new() directly and not
+         * the .new() of Gtk.Button. Ordering problem in registration of static functions? */
+        it('new()', null, function() {
+            var button = Gtk.Button.new();
+            button.label.should.equal(null);
+        });
+
+        it('new_with_argument(arg)', function() {
+            var button = Gtk.Button.new_with_label('myLabel312');
+            button.label.should.equal('myLabel312');
+        });
+
+    });
 
     describe('with properties', function() {
 
         it('derived from interface', function() {
-            var box = new gtk.Box( { orientation : gtk.Orientation.vertical } );
-            box.orientation.should.equal(gtk.Orientation.vertical); 
+            var box = new Gtk.Box( { orientation : Gtk.Orientation.vertical } );
+            box.orientation.should.equal(Gtk.Orientation.vertical);
         });
     });
+
 });
 
